0934fd33f01cb97656c28cef5250b8a5a2766d31
Erick Antillón <jpj007.ek@gmail.com>
2020-12-17 22:12:50 -0600

    Added extractRange

--- testEDD.py
+++ testEDD.py
@@ -9,39 +9,43 @@ import Storage  as j
 j.dropAll()
 
 # test Databases CRUD
-print(j.createDatabase('db1'))      # 0 
-print(j.createDatabase('db1'))      # 2
-print(j.createDatabase('db4'))      # 0
-print(j.createDatabase('db5'))      # 0
-print(j.createDatabase(0))          # 1
-print(j.alterDatabase('db5','db1')) # 3
-print(j.alterDatabase('db5','db2')) # 0
-print(j.dropDatabase('db4'))        # 0
-print(j.showDatabases())            # ['db1','db2']
+print(j.createDatabase('db1'))  # 0
+print(j.createDatabase('db1'))  # 2
+print(j.createDatabase('db4'))  # 0
+print(j.createDatabase('db5'))  # 0
+print(j.createDatabase(0))  # 1
+print(j.alterDatabase('db5', 'db1'))  # 3
+print(j.alterDatabase('db5', 'db2'))  # 0
+print(j.dropDatabase('db4'))  # 0
+print(j.showDatabases())  # ['db1','db2']
 
 # test Tables CRUD
-print(j.createTable('db1','tb4',3))     # 0
-print(j.createTable('db1','tb4',3))     # 3
-print(j.createTable('db1','tb1',3))     # 0
-print(j.createTable('db1','tb2',3))     # 0
+print(j.createTable('db1', 'tb4', 3))  # 0
+print(j.createTable('db1', 'tb4', 3))  # 3
+print(j.createTable('db1', 'tb1', 3))  # 0
+print(j.createTable('db1', 'tb2', 3))  # 0
 
-print(j.alterAddPK('db1','tb1',[0,1]))    # 0
-print(j.showTables('db1'))              # ['tb1', 'tb2']
+print(j.alterAddPK('db1', 'tb1', [0, 1]))  # 0
+print(j.showTables('db1'))  # ['tb1', 'tb2']
 # print(j.alterDropPK('db1','tb1'))       # 0
-print(j.alterDropPK('db1','tb2'))       # 4
-print(j.insert('db1','tb1',[1,'hola',1])) #0
-print(j.insert('db1','tb1',[2,'hola',1])) #0
-print(j.insert('db1','tb1',[3,'hola',1])) #0
-print(j.insert('db1','tb1',[4,'hola',2,5])) #5
-print(j.insert('db1','tb1',[3,'hola',1])) #4
+print(j.alterDropPK('db1', 'tb2'))  # 4
+print(j.insert('db1', 'tb1', [1, 'A', 1]))  # 0
+print(j.insert('db1', 'tb1', [2, 'B', 3]))  # 0
+print(j.insert('db1', 'tb1', [3, 'C', 4]))  # 0
+print(j.insert('db1', 'tb1', [4, 'D', 6]))  # 0
+print(j.insert('db1', 'tb1', [5, 'E', 1]))  # 0
+# print(j.insert('db1', 'tb1', [4, 'hola', 2, 5]))  # 5
+# print(j.insert('db1', 'tb1', [3, 'hola', 1]))  # 4
 
-print(j.extractTable("db1", "tb1"))
-print(j.extractRow("db1","tb1",[1,"hola"]))
-print(j.extractRow("db1","tb1",[3,"hola"]))
-print(j.delete("db1","tb1",[1,'hola']))   #0
-print(j.extractTable("db1", "tb1"))
-print(j.update("db1","tb1",{1: 'probando update'}, ['2','hola'])) #0
-print(j.update("db1","tb1",{1: 'probando update', 0:'2'}, ['3','hola'])) #1
-print(j.extractTable("db1", "tb1"))         #[]
-print(j.loadCSV("./tb1.csv","db1","tb5"))
-print(j.extractTable("db1", "tb5"))         #[0,0,0,0,0]
+# print(j.extractTable("db1", "tb1"))
+# print(j.extractRow("db1","tb1",[1,"hola"]))
+# print(j.extractRow("db1","tb1",[3,"hola"]))
+# print(j.delete("db1","tb1",[1,'hola']))   #0
+# print(j.extractTable("db1", "tb1"))
+# print(j.update("db1","tb1",{1: 'probando update'}, ['2','hola'])) #0
+# print(j.update("db1","tb1",{1: 'probando update', 0:'2'}, ['3','hola'])) #1
+# print(j.extractTable("db1", "tb1"))         #[]
+# print(j.loadCSV("./tb1.csv","db1","tb5"))
+# print(j.extractTable("db1", "tb5"))         #[0,0,0,0,0]
+
+print(j.extractRangeTable("db1", "tb1", 1, "A", "C"))
